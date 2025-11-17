710bc78fb303834005132843e7d0c6c5886fe828
Marco-Montelongo <marco.montelongo@ingenieria.unam.edu>
2025-11-17 07:16:03 +0000

    Esquema general del proyecto, public, data, src y un primer formulario de prueba

---
+++ src/model/exercisesModel.js
@@ -0,0 +1,12 @@
+export const menuData = [
+  {
+    title: "1. Elaboración del problema",
+    items: [
+      "1.1 Identificación de la necesidad",
+      "1.2 Definir el problema",
+      "1.3 Búsqueda de información",
+      "1.4 Especificaciones y requerimientos",
+      "1.5 Plan de trabajo"
+    ]
+  }
+];
